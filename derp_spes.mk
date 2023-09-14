f8e5dc5e167c019196f089fe429f0876bf6e16e2
fadlyas07 <mhmmdfdlyas@gmail.com>
2023-09-14 16:33:54 +0700

    spes: Init:DerpFest-AOSP bringup!
    Signed-off-by: fadlyas07 <mhmmdfdlyas@gmail.com>

--- derp_spes.mk
+++ derp_spes.mk
@@ -2,14 +2,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from common DerpFest-AOSP configuration
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_spes
+PRODUCT_NAME := derp_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -19,5 +19,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Target
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
+TARGET_NOT_USES_BLUR := true
+WITH_GMS := true
